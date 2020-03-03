207455bd4b781d588c15002c3d17127673631b35
USER <sdanilev@gmail.com>
2020-03-03 10:24:40 +0300

    Initial commit

---
+++ EconoMiC/EconoMiC/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+
+namespace EconoMiC
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButSignIn_Click(object sender, RoutedEventArgs e)
+        {
+            string Login = TextBoxLogin.Text;
+            string Password = TextBoxPassword.Password;
+        }
+
+        private void ButRegistrate_Click(object sender, RoutedEventArgs e)
+        {
+            TextBoxLogin.AppendText(" F ");
+        }
+    }
+}
